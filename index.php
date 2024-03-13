e886ebf218b307730fd95f75fe8f5632c042c986
RaphDoiz <doizi.raphael@gmail.com>
2024-03-13 16:46:36 +0100

    API verify SIREN

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,28 +8,51 @@
     <link rel="stylesheet" href="stylesheets/main.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
 </head>
+
 <body>
     <header class='bg-[#2d3d33] rounded-b-3xl overflow-hidden text-white'>
-        <div class='bg-[url("https://assets-global.website-files.com/64e48603c6d16b61230cf754/64ef94c674db03bf2b6fa88a_Banner%20BG-01.svg")] bg-cover min-h-[97.5vh]'>
+        <div
+            class='bg-[url("https://assets-global.website-files.com/64e48603c6d16b61230cf754/64ef94c674db03bf2b6fa88a_Banner%20BG-01.svg")] bg-cover min-h-[97.5vh]'>
             <?php include 'components/navbar.php'; ?>
-                <div class='flex items-center justify-center'>
+            <div class='flex items-center justify-center'>
                 <div class="grid grid-cols-2">
                     <div class="col-span-1 flex flex-col justify-center align-middle mx-auto">
                         <div>
                             <h1 class="text-5xl font-bold">UN CV,<br> UN STAGE</h1>
                             <p class="text-lg my-10">Créer , éditer , publier, Faites de vos CV une simplicité</p>
-                            <a href="view/signin.php" class="text-[#2d3d33] bg-white rounded-full p-4 font-bold">Commencer</a>
+                            <a href="view/signin.php"
+                                class="text-[#2d3d33] bg-white rounded-full p-4 font-bold">Commencer</a>
                         </div>
                     </div>
                     <div class="col-span-1">
-                    <i data-lucide="volume-2" class="my-class"></i>
-                        <img src="https://assets-global.website-files.com/64e48603c6d16b61230cf754/64ef94c674db03bf2b6fa88a_Banner%20BG-01.svg" alt="">
+                        <i data-lucide="volume-2" class="my-class"></i>
+                        <img src="https://assets-global.website-files.com/64e48603c6d16b61230cf754/64ef94c674db03bf2b6fa88a_Banner%20BG-01.svg"
+                            alt="">
                     </div>
                 </div>
-                </div>
+            </div>
         </div>
+
+        <p>
+            <?php
+                $siren = "COMPASS GROUP FRANCE"; //Mot-clé pour la recherche
+                $siren = str_replace(' ', '%20', $siren);
+                $endpoint = "https://recherche-entreprises.api.gouv.fr/search?q=$siren&page=1&per_page=1";
+                $curl = curl_init($endpoint);
+                curl_setopt_array($curl, [CURLOPT_RETURNTRANSFER => true]);
+                $data = json_decode(curl_exec($curl), true);
+
+                foreach($data['results'] as $result) {
+                    echo $result['siren'], '<br>';
+                }                
+                curl_close($curl);
+            ?>
+        </p>
+
+
     </header>
     <main class='min-h-screen'>
     </main>
 </body>
+
 </html>
