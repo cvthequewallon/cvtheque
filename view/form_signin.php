06760a3c6d470e7ad93b09dcf8212bc232dbb101
Tom Theret <tomnils.theret@gmail.com>
2024-03-13 14:34:51 +0100

    update href

--- view/form_signin.php
+++ view/form_signin.php
@@ -34,7 +34,7 @@
                 </form>
             </div>
             <hr class="my-5">
-            <a href="/view/signup.php" type="button" class="py-2.5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Créer un compte</a>
+            <a href="form_signup.php" type="button" class="py-2.5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Créer un compte</a>
         </div>
         </div>
     </main>
