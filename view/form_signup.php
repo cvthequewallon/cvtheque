7fb8f4b90b4ce90971405d21db0723f78668724e
RaphDoiz <doizi.raphael@gmail.com>
2024-03-20 08:44:37 +0100

    Sign Up with SIREN verif

--- view/form_signup.php
+++ view/form_signup.php
@@ -138,7 +138,7 @@
                 <div class="mb-5">
                     <label for="siren" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                         SIREN</label>
-                    <input type="number" id="siren" name="siren"
+                    <input type="number" id="siren" name="siren" minlength="9"maxlength="9"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required />
                 </div>
