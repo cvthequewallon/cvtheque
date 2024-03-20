037876bc9d0822246f8b8b666de3908dcb0cc65d
RaphDoiz <doizi.raphael@gmail.com>
2024-03-20 14:41:47 +0100

    Create CGU for the forms

--- view/formSignUp.php
+++ view/formSignUp.php
@@ -127,7 +127,7 @@
                             required />
                     </div>
                     <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">J'accepte
-                        les <a href="#" class="text-blue-700">conditions d'utilisation</a></label>
+                        les <a href="../cgu.html" target="_blank" class="text-blue-700">conditions d'utilisation</a></label>
                 </div>
                 <button type="submit" name="submitStudent" value="submitStudent"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Créer
@@ -202,7 +202,7 @@
                             required />
                     </div>
                     <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">J'accepte
-                        les <a href="#" class="text-blue-700">conditions d'utilisation</a></label>
+                        les <a href="../cgu.html" target="_blank" class="text-blue-700">conditions d'utilisation</a></label>
                 </div>
                 <button type="submit" name="submitCompany"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
